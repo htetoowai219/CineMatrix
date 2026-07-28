203683f54fe56821d1d0d88f742771617fde1342
Htet Oo Wai <mghtetoowai@gmail.com>
2026-07-28 21:03:10 +0630

    feat: add user login and logout controllers

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 
-import { registerUserController } from "../controllers/userAuth.controller";
+import {
+  loginUserController,
+  logoutUserController,
+  registerUserController,
+} from "../controllers/userAuth.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUserController);
+router.post("/login", loginUserController);
+router.delete("/logout", logoutUserController);
 
 export default router;
